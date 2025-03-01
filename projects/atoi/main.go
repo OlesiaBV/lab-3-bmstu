665ee6805489f521fb2967eb3eb895b49c0fac04
admin1 <admin1@Ubuntu.myguest.virtualbox.org>
2025-03-01 16:02:29 +0300

    Done!

--- projects/atoi/main.go
+++ projects/atoi/main.go
@@ -1,7 +1,36 @@
 package main
+import (
+	"fmt"
+	"strconv"
+)
+func squareDigits(number int) int {
+	isNegative := number < 0
+	if isNegative {
+		number = -number
+	}
+	numStr := strconv.Itoa(number)
+	result := ""
 
-import "fmt"
-
+	for _, char := range numStr {
+		n := int(char - '0')
+		squared := n * n
+		result += strconv.Itoa(squared)
+	}
+	finalResult, _ := strconv.Atoi(result)
+	if isNegative {
+		finalResult = -finalResult
+	}
+	return finalResult
+}
 func main() {
-	fmt.Println("Hello, world!!!")
+	var input string
+	fmt.Print("Введите число: ")
+	fmt.Scanln(&input)
+
+	number, err := strconv.Atoi(input)
+	if err != nil {
+		fmt.Println("Ошибка: введите корректное целое число.")
+		return
+	}
+	fmt.Println("Результат:", squareDigits(number))
 }
